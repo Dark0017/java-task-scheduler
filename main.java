0d7dab3b85ac31a9467cdb8381fa0f58e2885d56
Ayushman <51037059+Dark0017@users.noreply.github.com>
2021-05-04 00:58:02 +0530

    created basic class structures

---
+++ main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+enum Level{
+    LOW,
+    MEDIUM,
+    HIGH
+}
+
+class List {
+    public static Set<String> tags;
+}
+
+
+class Task {
+        String title;
+        String description;
+        Date deadline;
+        Level priority;
+        String tag;
+
+        private Boolean isComplete;
+        private Date created;
+
+
+        //create setTag method that selects tag from list object instance 
+
+
+}
+
+
+public class main {
+    public static void main(String[] args ){
+        System.out.println("Hello ppl");
+    }
+}
